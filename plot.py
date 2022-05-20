55dafeca94f3b66e00bfa2c8790cfa66ae994ed5
Roman Solař <rmnsolar@gmail.com>
2022-05-20 22:14:24 +0200

    Final documented version

--- plot.py
+++ plot.py
@@ -1,12 +1,80 @@
+"""
+This file contains the utilities for plotting the simulation outputs and
+reading data from solver output files.
+"""
 import matplotlib.pyplot as plt
 
 
-def plot(times, values, selected_species):
+def simple_plot(times, values, selected_species, xlog=False):
+    """
+    times ... list of timestamps
+    values ... dictionary of lists of the same dimension as times
+    selected_species ... keys from values to be plotted
+    xlog ... specifies if logarithmic x-axis
+
+    You should call plt.show() or plt.savefig(...) after running this method. Optionally, this call can be preceded
+    by other plot settings: such as plt.title(...).
+    """
     plt.yscale("log")  # set the y-axis in the plot to logarithmic scale
-    #plt.xscale("log")
+    if xlog: plt.xscale("log")
     plt.xlabel("time [s]")
-    plt.ylabel("number of particles (log scale)")
+    plt.ylabel("number of particles")
     for species in selected_species:
         plt.plot(times, values[species], label=species)
     plt.legend()
-    plt.show()
+
+
+def plot_with_EN(times, values, selected_species, ENs=None, xlog=False, ylim=None):
+    """
+    times ... list of timestamps
+    values ... dictionary of lists of the same dimension as times
+    selected_species ... keys from values to be plotted
+    Ens ... list of values of E/N at timestamps from times (must have the same dimension)
+    xlog ... specifies if logarithmic x-axis
+    ylim ... if specified, sets ylim for the values y-axis
+
+    You should call plt.show() or plt.savefig(...) after running this method. Optionally, this call can be preceded
+    by other plot settings: such as plt.title(...).
+    """
+    fig, ax1 = plt.subplots()
+
+    if xlog: ax1.set_xscale("log")
+    ax2 = ax1.twinx()
+
+    ax1.set_yscale('log')
+    ax1.set_xlabel('time [s]')
+    ax1.set_ylabel("species concentration [m$^{-3}$]")
+    ax2.set_ylabel('EN', color='b')
+
+    for species in selected_species:
+        ax1.plot(times, values[species], label=species)
+
+    if not ENs: ENs = values['EN']
+    ax2.plot(times, ENs, 'b', label="EN")
+    ax1.legend()
+    ax2.legend()
+    if ylim: ax1.set_ylim(ylim)
+
+
+def read_outfile(filename):
+    """
+    Reads an output file created by a method from solver.py and translates it into times, values in the same format
+    as the solvers from solver.py would return.
+    """
+    f = open(filename, 'r')
+    values = {}
+    for line in f.readlines():
+        spl = line.split(', ')
+        for item in spl:
+            item = item.strip()
+            name, val = item.split(': ')
+            if name not in values:
+                values[name] = []
+            values[name].append(float(val))
+    f.close()
+
+    # time has been parsed as a parameter of values
+    times = values['time']
+    del values['time']
+
+    return times, values
--- reactions.py
+++ reactions.py
@@ -1,8 +1,18 @@
+"""
+This file contains the class Reaction and specifies its methods.
+"""
 from collections import Counter
-from math import factorial
 
 
 class Reaction:
+    """
+    When instantiated from the input_parser, the attribute rate_fun will be None only if it cannot parse the string
+    following the character '!' after specifying the reaction in the input file.
+    Reactants and products are always known. They are dictionaries with keys being a subset of species and values being
+    natural numbers. E.g., reactants = {"Ar" : 1, "e" : 2}.
+
+    Attribute table_name is set by the input_parser if the reaction rate is described by a table.
+    """
     reactants = {}
     products = {}
     rate_fun = None
@@ -10,21 +20,26 @@ class Reaction:
     table_name = None
 
     def __init__(self, reactants, products, rate_fun=None):
-        # convert list to a dictionary, example:
+        # Counter: convert list to a dictionary, example:
         # ["Ar", "e", "e"] -> {"Ar" : 1, "e" : 2}
         self.reactants = Counter(reactants)
         self.products = Counter(products)
         self.rate_fun = rate_fun
 
     def compute_a(self, parameters):
+        """
+        Compute transition rate a_mu of the reaction R_mu.
+        """
         a = self.rate_fun(parameters)
         for reactant in self.reactants:
             for i in range(self.reactants[reactant]):
                 a *= parameters[reactant] - i
-            a *= 1 / factorial(self.reactants[reactant])
         return a
 
     def react(self, parameters, bulk):
+        """
+        Simulate executing bulk reactions: i.e., updating species concentrations.
+        """
         for reactant in self.reactants:
             parameters[reactant] -= self.reactants[reactant] * bulk
         for product in self.products:
